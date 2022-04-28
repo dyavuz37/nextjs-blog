b7cb3aaced8ba80ff53404efdd9d9e704eadb583
dyavuz37 <79080790+dyavuz37@users.noreply.github.com>
2022-04-28 04:39:35 +0300

    firstcommit

---
+++ pages/post/first-post.js
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/layout'
+import styles from './alert.module.css'
+import cn from 'classnames'
+export default function FirstPost() {
+  return (
+    <>
+ <Layout>
+    <Head>
+    <title>First Post</title>
+  <script src="https://connect.facebook.net/en_US/sdk.js" />
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      
+      </h2>
+      <YourComponent />
+      </Layout>
+    </>
+  )
+
+
+}
+const YourComponent = () => (
+    <Image
+      src="/images/profile.jpg" // Route of the image file
+      height={144} // Desired size with correct aspect ratio
+      width={144} // Desired size with correct aspect ratio
+      alt="Your Name"
+    />
+  )
